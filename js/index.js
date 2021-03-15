2fa65c3130d8be27f156bc22b59fe688af62c918
Sergei Kruglov <wqetris@gmail.com>
2021-03-15 18:14:18 +0800

    create form POST

--- js/index.js
+++ js/index.js
@@ -76,10 +76,73 @@ $(head).on('click', function(e) {
 });
 
 //4 acco menu
-var trigger = ('.menu-acco__trigger')
+var trigger = ('.menu-acco__head')
 triggerItem = ('.menu-acco__item')
 
 $(trigger).on('click', function(e) {
   e.preventDefault();
   $(this).closest(triggerItem).toggleClass(activeClass).siblings().removeClass(activeClass);
+});
+
+//5 order
+const myForm = document.querySelector('.form');
+const sendButton = document.querySelector('#sendBtn');
+
+sendButton.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if(validateForm(myForm)) {
+    const data = {
+      name: myForm.elements.name.value,
+      phone: myForm.elements.phone.value,
+      comment: myForm.elements.comment.value
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'http://httpbin.org/post');
+    xhr.send(JSON.stringify(data));
+    xhr.addEventListener('load', function() {
+      if (xhr.status >=400) {
+        overlay.classList.remove(hideClass);
+        overlayMessage.style.color = 'red';
+        overlayMessage.textContent = 'Отправить письмо не удалось, повторите запрос позже';
+      } else {
+        overlay.classList.remove(hideClass);
+      }
+    })
+  }
+});
+
+function validateForm(form) {
+  let valid = true;
+
+  if(!validateField(form.elements.name)) {
+    valid = false;
+  }
+  if(!validateField(form.elements.phone)) {
+    valid = false;
+  }
+  if(!validateField(form.elements.comment)) {
+    valid = false;
+  }
+  return valid;
+}
+
+function validateField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
+
+//6 overlay
+const overlay = document.querySelector('.form__overlay');
+const overlayMessage = document.querySelector('.overlay__message');
+const overlayButton = document.querySelector('.btn--overlay');
+overlayButton.addEventListener ('click', function(e) {
+  e.preventDefault();
+  overlay.classList.add(hideClass);
+});
+overlay.addEventListener("click", e => {
+  if (e.target === overlay) {
+    overlayButton.click();
+  }
 });
